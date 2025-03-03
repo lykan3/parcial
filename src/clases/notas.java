f3606c6028c8944b8b49b9d189ae10c412aceb69
LYKAN <LYKAN@DESKTOP-TIU35F5>
2025-03-03 07:43:17 -0500

    carga inicial

---
+++ src/clases/notas.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package clases;
+
+/**
+ *
+ * @author LYKAN
+ */
+public class notas {
+    
+    float nota1;
+    float nota2;
+    float nota3;
+
+    public notas(float nota1, float nota2, float nota3) {
+        this.nota1 = nota1;
+        this.nota2 = nota2;
+        this.nota3 = nota3;
+    }
+    
+    
+    
+}
